9852e596efac49205ef71b12762346e7aa5012a2
Wojtek Majewski <jumski@gmail.com>
2020-07-09 16:27:49 +0200

    Super simple example using subscriptions

--- frontend-cljs/src/app/core.cljs
+++ frontend-cljs/src/app/core.cljs
@@ -1,9 +1,37 @@
 (ns ^:dev/always app.core
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom]
+            [re-frame.core :as rf]))
+
+(defonce initial-state
+  {:counter 0})
+
+(rf/reg-event-fx
+  :initialize-db
+  (fn [& args]
+    {:db initial-state}))
+
+(rf/reg-event-db
+  :update-counter
+  (fn [db [_ delta]]
+    (update-in db [:counter] (partial + delta))))
+
+(rf/reg-sub
+  :counter
+  (fn [db _]
+    (:counter db)))
+
+(defn counter-component []
+  (let [counter (rf/subscribe [:counter])]
+    [:<>
+     [:button {:on-click #(rf/dispatch [:update-counter -1])} "-"]
+     [:span (str "counter =" @counter)]
+     [:button {:on-click #(rf/dispatch [:update-counter 1])} "+"]]))
+
+;;; META stuff
 
 (defn ui []
-  [:div "ziemniak"])
+  [:div [counter-component]])
 
 (defn render-ui []
   (let [dom-node (js/document.getElementById "app")]
@@ -12,4 +40,7 @@
 (defn ^:export reload! []
   (render-ui))
 
-(def ^:export main! render-ui)
+(defn ^:export main! []
+  (rf/dispatch-sync [:initialize-db])
+  (render-ui))
+
