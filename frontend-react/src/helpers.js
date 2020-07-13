d0b82f6272e35b12526d9a2bfa9aa9253fcee8b1
Wojtek Majewski <jumski@gmail.com>
2020-07-13 17:00:28 +0200

    Promote frontend-cljs to top dir, remove other crap

--- frontend-react/src/helpers.js
+++
@@ -1,9 +0,0 @@
-
-export function lastNumberOfDays(number) {
-  const today = new Date();
-  return [...Array(number).keys()].map(deltaDays => {
-    const date = new Date();
-    date.setDate(today.getDate() - deltaDays);
-    return date;
-  });
-}
